3fdb26653890c9f16fe420500afa01d3c1833bcb
Jorge Sánchez <jorge.sanchezroman@gmail.com>
2024-10-26 10:19:22 +0200

    new update before creating an app 2

--- staff/jorge-sanchez/playground/dorraymon/Concat.js
+++ staff/jorge-sanchez/playground/dorraymon/Concat.js
@@ -2,45 +2,23 @@ function Dorraymon() {
     this.length = 0
 }
 
-Dorraymon.prototype.concat = function (values) {
-
+Dorraymon.prototype.concat = function () {
     var result = new Dorraymon
-
-    // this -> names : // Dorraymon { 0: 'Jorge', 1: 'Carlos', 2: 'Isabel', 3: 'Elena', length: 4]
-    // values -> surnames : // Dorraymon // Dorraymon { 0: 'Sánchez', 1: 'Román', 2: 'Runk', 3: 'Ayala', length: 4
-    // result -> Dorraymon { length: 0}
-
-
-    // result[0] = this[0]   (result --> Dorraymon { 0: 'Jorge', length: 0)
-    // result.length++       (result --> Dorraymon { 0: 'Jorge', length: 1)
-
-    // result[1] = this[1]   (result --> Dorraymon { 0: 'Jorge', 1: 'Carlos', length: 1)
-    // result.length++       (result --> Dorraymon { 0: 'Jorge', 1: 'Carlos', length: 2)
-
-    // así sucesivamente, por todos los índices de this.
 
     for (var i = 0; i < this.length; i++) {
         result[i] = this[i]
         result.length++
     }
 
-    // Ahora pasamos a iterar desde el elemento 0 de value, pero continuando numeración de result.
+    for (var j = 0; j < arguments.length; j++) {
+        result[result.length] = arguments[j]
 
-    // result[5] = value[0]   
-    // result.length++
-
-    // así sucesivamente, por todos los índices de value
-
-    // result[7] = value[3]    (result --> Dorraymon { 0: 'Jorge', 1: 'Carlos', 2: 'Isabel', 3: 'Elena', 4: 'Sánchez', 5: 'Román', 6: 'Runk', 7: 'Ayala', length: 8}
-    // result.length++
-
-    for (var i = 0; i < values.length; i++) {
-        result[result.length] = values[i]
+        for (var i = 0; i < values.length; i++) {
+            result[result.length] = value[i]
+            result.length++
+        }
         result.length++
     }
-
-
-    // return result
 
     return result
 
@@ -62,11 +40,21 @@ surnames[2] = 'Runk'
 surnames[3] = 'Ayala'
 surnames.length = 4
 
-var complet = names.concat(surnames)
+var nicknames = new Dorraymon
+nicknames[0] = 'Coque'
+nicknames[1] = 'Caico'
+nicknames[2] = 'Beby'
+nicknames[3] = 'Helen'
+nicknames.length = 4
+
+var complet = names.concat(surnames, nicknames)
 
 console.log(names)
 // Dorraymon { 0: 'Jorge', 1: 'Carlos', 2: 'Isabel', 3: 'Elena', length: 4]
 console.log(surnames)
-// Dorraymon { 0: 'Sánchez', 'Román', 'Runk', 'Ayala', length: 4]
+// Dorraymon { 0: 'Sánchez', 1: 'Román', 2: 'Runk', 3: 'Ayala', length: 4]
+console.log(nicknames)
+// Dorraymon { 0: 'Coque', 1: 'Caico', 2: 'Beby', 3: 'Helen'}
 console.log(complet)
-// Dorraymon { 0:'Jorge', 1: 'Carlos', 2: 'Isabel', 3: 'Elena', 4: 'Sánchez', 5: 'Román', 'Runk', 'Ayala']
+// Dorraymon { 0:'Jorge', 1: 'Carlos', 2: 'Isabel', 3: 'Elena', 4: 'Sánchez', 5: 'Román', 6: 'Runk', 7:'Ayala', 8: 'Coque', 9: 'Caico', 10: 'Beby', 11: 'Helen'}
+
